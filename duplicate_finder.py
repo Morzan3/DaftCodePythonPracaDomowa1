b76393849fdf6579182f7968dfbe618e763dae0a
Morzan3 <ignacy3@gmail.com>
2017-03-14 23:00:48 +0100

    New duplicate finder implementation

--- duplicate_finder.py
+++ duplicate_finder.py
@@ -1,6 +1,7 @@
 import os
 import itertools
 import filecmp
+from pprint import pprint
 from file_hasher import get_hash
 
 
@@ -33,16 +34,37 @@ def duplicate_finder(topdir=None):
                             )
             data[size] = size_list
 
-    # TODO Miejsce, w którym nie byłem do końca pewien jak należy dokonać porównania par plików.
-    # Najprostrze jest po prostu wygenerowanie wszystkich możliwych par o danym hashu
-    # a następnie porównywać każdą z par jednak wtedy może dojść do niepotrzebnego porównania bo
-    # jeśli plik A jest taki sam jak plik B a plik B jest taki sam jak plik C to plik A jest taki
-    # sam jak plik C i możemy to stwierdzić bez porównania. Nie było jednak pewien czy
-    # takie zaimplementowanie porównania było konieczne.
     for files_to_check in hashes.values():
+        file_corelation = {}
+        for file in files_to_check:
+            file_corelation[file] = set()
+
+        # Dla każdego hasha tworzę słownik {lokalizaja pliku: set plików z którymi jest powiązany)
+        # Tworzę wszystkie możliwe pary plików i po kolei sprawdzam:
+        # jeśli plik2 znajduje się w zbiorze wartości pliku1 to nie musze sprawdzać tylko pisze, że zachodzi kolizja
+        # jesli nie znajduje się w zbiorze to porówuje te pliki.
+        # W przypadku gdy pliki są identyczne to dodaję plik2 to setu pliku1, dodaję wszystkie pliki powiązane z plikiem 2
+        # do setu pliku1 a następnie wszystkie te wartości kopiuje do setów plików pokrewnych do pliku1.
+        # Rozwiązanie wydaje mi się dosyć zawiłe więc chętnie poznał bym jakieś lepsze :)
+
         for file1, file2 in itertools.combinations(files_to_check, 2):
-            if filecmp.cmp(file1, file2):
+            if file2 not in file_corelation[file1]:
+                if filecmp.cmp(file1, file2):
+                    file_corelation[file1].add(file2)
+                    if file_corelation[file2]:
+                        file_corelation[file1] = file_corelation[file1].union(file_corelation[file2])
+
+                    for file in file_corelation[file1]:
+                        file_corelation[file] = file_corelation[file].union(file_corelation[file1])
+
+                    file_corelation[file2].add(file1)
+
+                    print('File collision for following files:\n\t{}\n\t{}'.format(file1, file2))
+
+            else:
                 print('File collision for following files:\n\t{}\n\t{}'.format(file1, file2))
+
+        pprint(file_corelation)
 
     return data, hashes
 
